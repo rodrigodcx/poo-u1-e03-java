90e3fcee52e4284ef200a1e63e2baf3c43a77e7a
Rodrigo Vilar <rodrigovilar@dcx.ufpb.br>
2020-09-20 00:58:34 -0300

    Teste para o Exercício 3

--- src/test/java/com/example/project/TestCupomFiscal.java
+++ src/test/java/com/example/project/TestCupomFiscal.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 
 import java.io.*;
 
-public class TestExercicio {
+public class TestCupomFiscal {
 
 	private String BREAK = System.lineSeparator();
 
 	@Test
-	public void exercicio01() {
+	public void lojaCompleta() {
 		rodarTestarOutput("Arcos Dourados Com. de Alimentos LTDA" + BREAK + 
 				"Av. Projetada Leste, 500 EUC F32/33/34" + BREAK + 
 				"Br. Sta Genebra - Campinas - SP" + BREAK + 
@@ -20,34 +20,66 @@ public class TestExercicio {
 				"CNPJ: 42.591.651/0797-34" + BREAK + 
 				"IE: 244.898.500.113" + BREAK);
 	}
+
+	@Test
+	public void nomeVazio() {
+		CupomFiscal.NOME_LOJA = "";
+		rodarTestarOutput("O campo nome da loja é obrigatório");
+		CupomFiscal.NOME_LOJA = "Arcos Dourados Com. de Alimentos LTDA";
+	}
 	
 	@Test
-	public void exercicio02_tudoVazio() {
-		CupomFiscal.NOME_LOJA = "";
+	public void logradouroVazio() {
 		CupomFiscal.LOGRADOURO = "";
-		CupomFiscal.NUMERO = 0;
-		CupomFiscal.COMPLEMENTO = "";
-		CupomFiscal.BAIRRO = "";
-		CupomFiscal.MUNICIPIO = "";
-		CupomFiscal.ESTADO = "";
-		CupomFiscal.CEP = "";
-		CupomFiscal.TELEFONE = "";
-		CupomFiscal.OBSERVACAO = "";
-		CupomFiscal.CNPJ = "";
-		CupomFiscal.INSCRICAO_ESTADUAL = "";
-		
-		rodarTestarOutput("" + BREAK + 
-				", 0 " + BREAK + 
-				" -  - " + BREAK + 
-				"CEP: Tel " + BREAK + 
-				"" + BREAK + 
-				"CNPJ: " + BREAK + 
-				"IE: " + BREAK);
+		rodarTestarOutput("O campo logradouro do endereço é obrigatório");
+		CupomFiscal.LOGRADOURO = "Av. Projetada Leste";
 	}
 
 	@Test
+	public void numeroZero() {
+		CupomFiscal.NUMERO = 0;
+		rodarTestarOutput("Arcos Dourados Com. de Alimentos LTDA" + BREAK +
+				"Av. Projetada Leste, s/n EUC F32/33/34" + BREAK +
+				"Br. Sta Genebra - Campinas - SP" + BREAK +
+				"CEP:13080-395 Tel (19) 3756-7408" + BREAK +
+				"Loja 1317 (PDP)" + BREAK +
+				"CNPJ: 42.591.651/0797-34" + BREAK +
+				"IE: 244.898.500.113" + BREAK);
+
+		CupomFiscal.NUMERO = 500;
+	}
+	
+	@Test
+	public void municipioVazio() {
+		CupomFiscal.MUNICIPIO = "";
+		rodarTestarOutput("O campo município do endereço é obrigatório");
+		CupomFiscal.MUNICIPIO = "Campinas";
+	}
+
+	@Test
+	public void estadoVazio() {
+		CupomFiscal.ESTADO = "";
+		rodarTestarOutput("O campo estado do endereço é obrigatório");
+	    CupomFiscal.ESTADO = "SP";
+	}
+	
+	@Test
+	public void cnpjVazio() {
+		CupomFiscal.CNPJ = "";
+		rodarTestarOutput("O campo CNPJ da loja é obrigatório");
+	    CupomFiscal.CNPJ = "42.591.651/0797-34";
+	}
+
+	@Test
+	public void inscricaoEstadualVazia() {
+		CupomFiscal.INSCRICAO_ESTADUAL = "";
+		rodarTestarOutput("O campo inscrição estadual da loja é obrigatório");
+		CupomFiscal.INSCRICAO_ESTADUAL = "244.898.500.113";
+	}
+	
+	@Test
 	public void exercicio02_Customizado() {
-		//Defina seus prÃ³prios valores para as variÃ¡veis a seguir 
+		//Defina seus próprios valores para as variáveis a seguir 
 		CupomFiscal.NOME_LOJA = "";
 		CupomFiscal.LOGRADOURO = "";
 		CupomFiscal.NUMERO = 0;
